1a82c152659ec32731c17a2fcf2abd48637762d4
sseul <jjwisg@naver.com>
2018-12-20 16:57:28 +0900

    comm

--- systemTest1/src/Test1/SalesReg.java
+++ systemTest1/src/Test1/SalesReg.java
@@ -1,28 +1,39 @@
 package Test1;
 
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
-import javax.swing.JButton;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 public class SalesReg extends JPanel {
-	DefaultTableModel firstTabModel,secTabModel,thrTabModel;
+	JLabel lab;
+	DefaultTableModel firstTabModel, secTabModel, thrTabModel;
 	JTable firstTab, secTab, thrTab;
-	JScrollPane firstSc,secSc,thrSc;
+	JScrollPane firstSc, secSc, thrSc;
 
 	public SalesReg() {
-		setLayout(new GridLayout(0, 1, 0, 0));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		// 1
 		JPanel p1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) p1.getLayout();
+		flowLayout.setHgap(10);
+		flowLayout.setVgap(10);
+		flowLayout.setAlignment(FlowLayout.LEFT);
 		add(p1);
-		JLabel lblSalesReg = new JLabel("판매등록");
-		lblSalesReg.setBounds(201, 5, 48, 15);
-		p1.add(lblSalesReg);
+		lab = new JLabel("판매등록");
+		lab.setFont(new Font("굴림", Font.PLAIN, 18));
+
+		p1.add(lab);
 
 		// 2
 		String firstTabName[] = { "판매일자", "총판매금액" };
@@ -30,25 +41,44 @@ public class SalesReg extends JPanel {
 		firstTabModel = new DefaultTableModel(firstData, firstTabName);
 		firstTab = new JTable(firstTabModel);
 		firstSc = new JScrollPane(firstTab);
+		firstSc.setPreferredSize(new Dimension(450, 100));
 		add(firstSc);
-		
+
 		// 3
-		String secTabName[] = { "구분","품번","색상","사이즈","판매단가","재고","수량","실판매금액","등록" };
-		Object secData[][] = { { "판매","000","선택","선택","88000","3","1","88000","등록" } };
+		String secTabName[] = { "구분", "품번", "색상", "사이즈", "판매단가", "재고", "수량", "실판매금액", "등록" };
+		Object secData[][] = { { "판매", "000", "선택", "선택", "88000", "3", "1", "88000", "등록" } };
 		secTabModel = new DefaultTableModel(secData, secTabName);
 		secTab = new JTable(secTabModel);
 		secSc = new JScrollPane(secTab);
+		secSc.setPreferredSize(new Dimension(450, 100));
 		add(secSc);
-		
+
 		// 4
-		String thrTabName[] = { "구분","품번","색상","사이즈","판매단가","수량","실판매금액","삭제" };
-		Object thrData[][] = { { "판매","1","BK","S","5000","1","5000","삭제" },
-				{ "반품","2","WH","M","7000","1","7000","삭제" },
-				{ "판매","3","BK","L","6000","1","6000","삭제" }};
+		String thrTabName[] = { "구분", "품번", "색상", "사이즈", "판매단가", "수량", "실판매금액", "삭제" };
+		Object thrData[][] = { { "판매", "1", "BK", "S", "5000", "1", "5000", "삭제" },
+				{ "반품", "2", "WH", "M", "7000", "1", "7000", "삭제" },
+				{ "판매", "3", "BK", "L", "6000", "1", "6000", "삭제" } };
 		thrTabModel = new DefaultTableModel(thrData, thrTabName);
 		thrTab = new JTable(thrTabModel);
 		thrSc = new JScrollPane(thrTab);
-		add(thrSc);		
-	}
+		add(thrSc);
+		
+		// table center align
+		DefaultTableCellRenderer tCellRenderer = new DefaultTableCellRenderer();
+		tCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
 
+		TableColumnModel t1ColModel = firstTab.getColumnModel();
+		TableColumnModel t2ColModel = secTab.getColumnModel();
+		TableColumnModel t3ColModel = thrTab.getColumnModel();
+
+		for (int i = 0; i < t1ColModel.getColumnCount(); i++)
+			t1ColModel.getColumn(i).setCellRenderer(tCellRenderer);
+
+		for (int i = 0; i < t2ColModel.getColumnCount(); i++)
+			t2ColModel.getColumn(i).setCellRenderer(tCellRenderer);
+
+		for (int i = 0; i < t3ColModel.getColumnCount(); i++)
+			t3ColModel.getColumn(i).setCellRenderer(tCellRenderer);
+
+	}
 }
--- systemTest1/src/Test1/StockSearch.java
+++ systemTest1/src/Test1/StockSearch.java
@@ -1,6 +1,8 @@
 package Test1;
 
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -8,9 +10,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-
+import javax.swing.table.TableColumnModel;
 import javax.swing.JRadioButton;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 
@@ -21,25 +26,31 @@ public class StockSearch extends JPanel {
 	JScrollPane firstSc, secSc;
 	private JRadioButton[] rdbtn = new JRadioButton[2];
 	private JButton btnSearch;
-	String[] radioText = {"매장별", "사이즈별"};
+	String[] radioText = { "매장별", "사이즈별" };
 	private JRadioButton rdbtnH;
 	private JRadioButton rdbtnJj;
 	private JRadioButton rdbtnKk;
 
 	public StockSearch() {
-		setLayout(new GridLayout(4, 1, 0, 0));
-		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
 		// 1
 		JPanel p1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) p1.getLayout();
+		flowLayout.setHgap(10);
+		flowLayout.setVgap(10);
+		flowLayout.setAlignment(FlowLayout.LEFT);
 		add(p1);
 		lab = new JLabel("재고조회");
-		lab.setBounds(100, 100, 450, 300);
 		p1.add(lab);
-		
+		lab.setFont(new Font("굴림", Font.PLAIN, 18));
+
 		// 2
 		JPanel p2 = new JPanel();
-		add(p2);		
-			// radio*
+		FlowLayout flowLayout_1 = (FlowLayout) p2.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.RIGHT);
+		add(p2);
+		// radio*
 		MyItemListener itemlis = new MyItemListener(); // 아이템 감시자
 		ButtonGroup g = new ButtonGroup(); // 라디오 버튼 묶을 그룹
 
@@ -50,29 +61,43 @@ public class StockSearch extends JPanel {
 
 			rdbtn[i].addItemListener(itemlis); // 감시자 부착
 		}
-		rdbtn[0].setSelected(true); // 해당 버튼이 선택된 상태	
-		
-			// btn
+		rdbtn[0].setSelected(true); // 해당 버튼이 선택된 상태
+
+		// btn
 		btnSearch = new JButton("조회");
 		p2.add(btnSearch);
-		
+
 		// 3
 		String firstTabName[] = { "품 번", "판매단가" };
 		Object firstData[][] = { { "181001", "59,000" } };
 		firstTabModel = new DefaultTableModel(firstData, firstTabName);
 		firstTab = new JTable(firstTabModel);
 		firstSc = new JScrollPane(firstTab);
+		firstSc.setPreferredSize(new Dimension(450, 100));
 		add(firstSc);
-		
+
 		// 4
-		String secTabName[] = { "색상","사이즈","매장코드","매장명","전화번호","재고" };
+		String secTabName[] = { "색상", "사이즈", "매장코드", "매장명", "전화번호", "재고" };
 		Object secData[][] = { { "BK", "S", "H0001", "본사창고", "031-777-1111", "5" },
 				{ "BK", "M", "H0001", "본사창고", "031-777-1111", "4" },
-				{ "BK", "S", "S3210", "뉴코아광명", "010-8888-8888", "3" }};
+				{ "BK", "S", "S3210", "뉴코아광명", "010-8888-8888", "3" } };
 		secTabModel = new DefaultTableModel(secData, secTabName);
 		secTab = new JTable(secTabModel);
 		secSc = new JScrollPane(secTab);
 		add(secSc);
+
+		// table center align
+		DefaultTableCellRenderer tCellRenderer = new DefaultTableCellRenderer();
+		tCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+		TableColumnModel t1ColModel = firstTab.getColumnModel();
+		TableColumnModel t2ColModel = secTab.getColumnModel();
+
+		for (int i = 0; i < t1ColModel.getColumnCount(); i++)
+			t1ColModel.getColumn(i).setCellRenderer(tCellRenderer);
+
+		for (int i = 0; i < t2ColModel.getColumnCount(); i++)
+			t2ColModel.getColumn(i).setCellRenderer(tCellRenderer);
 	}
 
 	// radio listener
